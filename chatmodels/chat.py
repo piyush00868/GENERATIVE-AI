96f3690b35116745650eb88a838aee3a7644c381
piyush00868 <pk2857636@gmail.com>
2026-08-25 16:29:46 +0530

    Add chat model initialization and response invocation in chat.py

--- chatmodels/chat.py
+++ chatmodels/chat.py
@@ -1,2 +1,12 @@
 from dotenv import load_dotenv
-load_dotenv()  # Load environment variables from .env file  
+from langchain.chat_models import init_chat_model
+
+load_dotenv()
+
+model = init_chat_model(
+    "gemini-3-flash-preview",
+    model_provider="google_genai"
+)
+
+response = model.invoke("What is Python?")
+print(response.text)
