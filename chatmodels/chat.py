6aec892411efe0ec015bc718ee5d2254ac049c14
piyush00868 <pk2857636@gmail.com>
2026-09-15 13:48:18 +0530

    Update chat model to use ChatGroq for improved response generation

--- chatmodels/chat.py
+++ chatmodels/chat.py
@@ -1,8 +1,8 @@
 from dotenv import load_dotenv
-from  langchain_mistralai import ChatMistralAI
+from  langchain_groq import ChatGroq
 
 load_dotenv()  # Load environment variables from .env file  
 
-model = ChatMistralAI(model="mistral-small-latest", temperature=0.1)
-response = model.invoke("What is python and use cases of it?")
+model = ChatGroq(model="openai/gpt-oss-120b")
+response = model.invoke("What is python and use cases of it?" )
 print(response.text)
